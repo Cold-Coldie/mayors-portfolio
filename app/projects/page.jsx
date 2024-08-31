0dc728dfde897ac0c8537e946946d8a88bae5620
Erioluwa Ogedengbe <ogedengbeoluwasemilogo@gmail.com>
2024-08-31 16:38:37 +0100

    Change site name

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -16,7 +16,7 @@ export const metadata = {
     title: "Projects Page | About Oyewumi Mayowa | Data Analyst & Scientist",
     description:
       "Discover the journey of Oyewumi Mayowa, a passionate data analyst and scientist.",
-    url: "https://mayor-portfolio.netlify.app/",
+    url: "https://mayors-portfolio.netlify.app/",
     images: [
       {
         url: "/static/images/profile-picture.jpg",
